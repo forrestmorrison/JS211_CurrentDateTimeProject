d37cf502aeb2fa0e4e4313f8bd31d83275428081
forrestmorrison <forrestmorrison@gmail.com>
2022-01-19 09:51:51 -0600

    completed part one functions

--- main.js
+++ main.js
@@ -5,6 +5,7 @@
 //          PART ONE
 // ***************************
 // Write a JavaScript program to display the current day and time, start with:
+
 console.log(new Date)
 
 const displayDate = () => {
@@ -16,11 +17,22 @@ const displayDate = () => {
 
 // Write a JavaScript program to convert a number to a string.
 
-
+const numberToString = (myNumber) => {
+  const stringInt = myNumber.toString();
+  return stringInt;
+}
+const convertedNumber = numberToString(23);
+console.log(convertedNumber);
 
 // Write a JavaScript program to convert a string to the number.
 
+const stringToNumber = (myString) => {
+  const numInt = parseInt(myString);
+  return numInt;
+}
 
+const convertedString = stringToNumber(33);
+console.log(convertedString);
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
   // * Boolean
@@ -30,21 +42,62 @@ const displayDate = () => {
   // * NaN
   // * String
   
+const printDataType = (data) => {
+  const dataType = typeof(data);
+  return dataType;
+}
 
+const myBoolean = printDataType(true);
+console.log(myBoolean);
   
 // Write a JavaScript program that adds 2 numbers together.
 
+const addNumbers = (num1, num2) => {
+  const sum = num1 + num2;
+  return sum;
+}
 
+const numSum = addNumbers(5, 5)
+console.log(numSum);
 
 // Write a JavaScript program that runs only when 2 things are true.
 
+const doubleTrue = (input) => {
+  if (input > 10 && input < 50) {
+    return "Two Things are True!";
+  } else {
+    return "Something is False!";
+  }
+}
 
+const isDoubleTrue = doubleTrue(25);
+console.log(isDoubleTrue);
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
 
+const singleTrue = (input) => {
+  if (input > 20 || input === 10) {
+    return "One thing is true!"
+  } else {
+    return "Neither is true!"
+  }
+}
 
+const isSingleTrue = singleTrue(10);
+console.log(isSingleTrue);
 
 // Write a JavaScript program that runs when both things are not true.  
+
+const doubleFalse = (input) => {
+  if (input !== 25 && input !== 35) {
+    return "Neither is true!"
+  } else {
+    return "Something is true!"
+  }
+}
+
+const isDoubleFalse = doubleFalse(10);
+console.log(isDoubleFalse);
 
 // ***************************
 //         PART TWO
